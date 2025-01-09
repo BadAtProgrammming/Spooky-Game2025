bcf05bdd9c41d7727e097816ed6c252926a17472
YRG06 <samuel.soderberg@elev.ga.lbs.se>
2025-01-09 16:18:23 +0100

    Player movement (Temporär)

---
+++ Assets/Scripts/Samuel/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    float horz;
+    float vert;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horz = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+       
+
+        rb.velocity = new Vector2(rb.velocity.x, vert * 7);
+        
+        rb.velocity = new Vector2(horz * 7, rb.velocity.y);
+    }
+}
