485ea27130efe9f99166e8f2cbbdd87652537f90
BadAtProgrammming <benjamin.nord@elev.ga.lbs.se>
2025-01-13 13:59:50 +0100

    camera

--- Assets/Scripts/Benjamin/Controls.cs
+++ Assets/Scripts/Benjamin/Controls.cs
@@ -5,16 +5,9 @@ using UnityEngine.InputSystem;
 
 public class Controls : MonoBehaviour
 {
-    // Player (Gameplay) prob garbage code
-    public InputActionReference m_MoveUp;public InputActionReference m_MoveDown;public InputActionReference m_MoveLeft;public InputActionReference m_MoveRight;public InputActionReference m_Interact;public InputActionReference m_OpenUI;
-
-
-    // Player (Menu) 
-    public InputActionReference m_MenuUp;public InputActionReference m_MenuDown;public InputActionReference m_MenuSelect;public InputActionReference m_MenuReturn;public InputActionReference m_CloseUI;
-
-    /* Work In Progress for later
-    void RebindActionKey() 
+    public PlayerInputActions _input;
+    private void Start()
     {
+        _input = new PlayerInputActions();
     }
-    */
 }
