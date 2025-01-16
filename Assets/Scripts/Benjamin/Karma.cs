a56d9fef5dd481b170456c5e2eb0d36c76f41c3c
BadAtProgrammming <benjamin.nord@elev.ga.lbs.se>
2025-01-16 09:46:58 +0100

    karma

--- Assets/Scripts/Benjamin/Karma.cs
+++ Assets/Scripts/Benjamin/Karma.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Karma : MonoBehaviour
+public class Choice : MonoBehaviour
 {
     int KarmaPoints;
     [SerializeField] bool Ending;
