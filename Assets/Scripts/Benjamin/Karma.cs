580efdef5fb95598a6ba0967fd73a2c0f7a2d769
BadAtProgrammming <benjamin.nord@elev.ga.lbs.se>
2025-01-16 09:59:46 +0100

    moved files to sam folder and changed pc

--- Assets/Scripts/Benjamin/Karma.cs
+++ Assets/Scripts/Benjamin/Karma.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Choice : MonoBehaviour
+public class Karma : MonoBehaviour
 {
     int KarmaPoints;
     [SerializeField] bool Ending;
