989c3e7cdfe310b3e786faf9a24b7cf1a02a1209
Kemilcurry <119925693+Kemilcurry@users.noreply.github.com>
2025-01-17 12:43:01 +0100

    balls

--- Assets/Scripts/centerlight.cs
+++ Assets/Scripts/centerlight.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ingsort : MonoBehaviour
+public class centerlight : MonoBehaviour
 {
+    [SerializeField]
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.position = player.transform.position;
     }
 }
