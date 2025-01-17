dab8474eb60575942c55532d12ed16a0688ce34c
BadAtProgrammming <benjamin.nord@elev.ga.lbs.se>
2025-01-17 09:28:43 +0100

    cutscenes almost done

---
+++ Assets/Scripts/Benjamin/Cutscene.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cutscene : MonoBehaviour
+{
+    [SerializeField] GameObject[] cutscenes;
+    int currentcutscene;
+    bool On; // is script on or off?
+    // Start is called before the first frame update
+    void Start()
+    {
+        On = true;
+        currentcutscene = 0;
+        cutscenes[currentcutscene].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (On == true)
+        {
+            if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Backspace)) // press any of these buttons and it goes to the next cutscene and disables current cutscene image.
+            {
+                cutscenes[currentcutscene].SetActive(false);
+                currentcutscene += 1;
+                cutscenes[currentcutscene].SetActive(true);
+
+            }
+
+            if (currentcutscene >= 3) // if currentcutscene is 3 or higher go to main scene
+            {
+                SceneManager.LoadScene("Game Over");
+            }
+        }
+
+    }
+
+    /* stole my own code from another project, ignore;
+    public void PlayCutscenes() 
+    {
+
+        cutscenes[currentcutscene].SetActive(true);
+        On = true;
+
+    }
+    */ 
+}
